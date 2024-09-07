650a6e8c38062cddd5ec4848ce9f88143b7b7e5d
Denis Developer <denispereiraa55@gmail.com>
2024-09-07 09:01:19 -0300

    corrigindo conflito de merge

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -6,8 +6,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
+import { CategoriaProdutoAdminComponent } from './components/categoria-produto-admin/categoria-produto-admin.component';
 import { FormAddressComponent } from './components/form-address/form-address.component';
 import { FormCompanyComponent } from './components/form-company/form-company.component';
+import { ProdutoAdminComponent } from './components/produto-admin/produto-admin.component';
 import { ProdutoComponent } from './components/produto/produto.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { SubTitleComponent } from './components/sub-title/sub-title.component';
@@ -15,8 +17,6 @@ import { AdminHomeComponent } from './pages/admin-home/admin-home.component';
 import { CompanyManagementComponent } from './pages/company-management/company-management.component';
 import { MenuManagementComponent } from './pages/menu-management/menu-management.component';
 import { UsersComponent } from './pages/users/users.component';
-import { ProdutoAdminComponent } from './components/produto-admin/produto-admin.component';
-import { CategoriaProdutoAdminComponent } from './components/categoria-produto-admin/categoria-produto-admin.component';
 
 @NgModule({
   declarations: [
@@ -29,8 +29,8 @@ import { CategoriaProdutoAdminComponent } from './components/categoria-produto-a
     SubTitleComponent,
     ProdutoAdminComponent,
     CategoriaProdutoAdminComponent,
-    FormEmpresaComponent,
-    FormEnderecoComponent,
+    FormCompanyComponent,
+    FormAddressComponent,
   ],
   imports: [
     SharedModule,
